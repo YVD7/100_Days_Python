8eb850edbbb52e0426f69db1b05db393e9195d3c
YVD7 <yashdawande7@gmail.com>
2023-09-30 17:29:00 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,41 @@
+# print("Bye")
+# print("Hello " + input("what is your name?"))
+
+#a = len(input())
+# print(a)
+
+# name = "jack"
+# print(name)
+
+# name = "Angela"
+# print(name)
+
+# name = input("what is your name?")
+# length = len(name)
+# print(length)
+
+# a = input()
+# b = input()
+
+# c = a
+# a = b
+# b = c
+
+# print("a: ", b)
+# print("b: ", a)
+
+# 1. Create a greeting for your program
+print("Hello")
+name = input("yash : ")
+# 2. Ask the user for the city that the grew up in
+print("my i know in which city you grew up? \n")
+city = input("city name: ")
+print(city)
+
+# 3. Ask the user for name of a pet
+print("what is the name of your pet?\n")
+pet = input("pet name: ")
+print(pet)
+# 4. Combine the input name of their city and pet and show them their band name.
+print("your band name should be " + city +" "+pet)
+# 5. Make sure the input cursor shows on a new line
